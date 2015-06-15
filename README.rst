79e9d7bde1a4826cab1c0efa47079fe31898745b
Hao Liu <liuhao1990@gmail.com>
2015-06-14 20:17:49 -0400

    0.1.0

--- README.rst
+++ README.rst
@@ -1,1 +1,16 @@
-ddd
+# Test
+
+```
+python -m unittest tests/test.py
+```
+
+# Use
+
+```
+from china_stock import Stock
+
+symbol_list = ['sh000001', 'sh000002', 'sh601006']
+stocks = Stock(symbol_list)
+
+stock = stocks.get_data[0]
+```
--- setup.py
+++ setup.py
@@ -3,35 +3,13 @@ import codecs
 import os
 import re
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def find_version(*file_paths):
-    """Read the version number from a source file.
-    Why read it, and not import?
-    see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
-    """
-    # Open in Latin-1 so that we avoid encoding errors.
-    # Use codecs.open for Python 2 compatibility
-    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
-        version_file = f.read()
-
-    # The version line must have the form
-    # __version__ = 'ver'
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
 # Get the long description from the relevant file
 with codecs.open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name="china-stock",
-    version=find_version('china_stock', '__init__.py'),
+    version="0.1.1",
     description="china stock",
     long_description=long_description,
 

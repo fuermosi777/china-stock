3721b1f2281cf5cfcb53446b00a61ff959adddcc
Hao Liu <liuhao1990@gmail.com>
2015-07-11 17:32:01 -0400

    fix json python3 decode

--- chinastock/stock/basic.py
+++ chinastock/stock/basic.py
@@ -11,7 +11,7 @@ GET_STOCK_TODAY_URL = 'http://img1.money.126.net/data/hs/time/today/%s%s.json'
 def get_stock_today(code, exchange):
 	exchange = cs_service.exchange_to_num(exchange)
 	response = urlopen(GET_STOCK_TODAY_URL%(exchange, code))
-	data = json.loads(response.read())
+	data = json.loads(response.read().decode())
 	price = data['data']
 	date = data['date']
 	res = [[datetime.strptime(date + d[0], '%Y%m%d%H%M'), d[1], d[3]] for d in price]
